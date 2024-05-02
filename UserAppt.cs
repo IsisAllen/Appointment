b77be64fe7b740b77a13dbf344347978f53d1b9e
Jessica Jabin <jessica.jabin@marquette.edu>
2024-05-02 14:27:11 -0500

    Create UserAppt class

---
+++ UserAppt.cs
@@ -0,0 +1,23 @@
+﻿namespace Appointment;
+
+public class UserAppt
+{
+    // Definition of a public property named u, representing a User object.
+    public User u { get; set; }
+
+    // Definition of a public property named a, representing an App object.
+
+    public Appt a { get; set; }
+
+    // // Constructor method for the UserAppt class.
+
+    public UserAppt(User u, Appt a)
+    {
+        // Assigning the provided User object to the u property
+        this.u = u;
+        // Assigning the provided Appt object to the a property
+        this.a = a;
+    }
+
+
+}
